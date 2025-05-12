080415a2b62a3d4e614927be28e204ef4bbd3242
swamy527 <swamybandari527@gmail.com>
2025-05-12 07:57:08 +0530

    files added

---
+++ main.tf
@@ -0,0 +1,21 @@
+
+resource "aws_security_group" "manayg" {
+  name        = "yandi"
+  description = "all all traffic"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "yandi"
+  }
+}
